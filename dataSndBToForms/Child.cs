9a92801fb42ebd3ff3c7ea460fb544c3f6cfed1e
Muzamil <muzamilzubair987654@gmail.com>
2025-09-01 00:28:12 +0500

    Add project files.

---
+++ dataSndBToForms/Child.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataSndBToForms
+{
+    public partial class Child : Form
+    {
+        public Child()
+        {
+            InitializeComponent();
+        }
+        public Child(string name)
+        {
+            InitializeComponent();
+            this.Name = name;
+            txtName.Text = name;    
+        }
+        private void Child_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public string SendData()
+        {
+            return "Age of " + Name + " is " + txtAge.Text;
+        }
+    }
+}
---
+++ dataSndBToForms/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace dataSndBToForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(311, 108);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(320, 26);
+            this.txtName.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(311, 177);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(320, 41);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send to Child";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.Location = new System.Drawing.Point(311, 257);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(320, 26);
+            this.txtShow.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtShow;
+    }
+}
+
